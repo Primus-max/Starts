8b478c650a95b655a32cc31bd2cba9d46ecaa027
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-03-06 23:35:15 +0300

    Добавьте файлы проекта.

---
+++ Starts.cpp
@@ -0,0 +1,36 @@
+﻿//  Написать рекурсивную функцию, которая
+//  выводит N звезд в ряд, число N задает пользователь.Проиллюстрируйте работу функции примером.
+
+#include <iostream>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void PrintStars(int N);
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N;
+
+    cout << "Введите число N чтобы увидеть звёзды: ";
+    cin >> N;
+
+    cout << "Вывод " << N << " звезд в ряд: ";
+    PrintStars(N);
+    cout << endl;
+
+    return 0;
+}
+
+void PrintStars(int N) {
+    if (N <= 0) {
+        return;
+    }
+    else {
+        cout << "* ";
+        PrintStars(N - 1);
+    }
+}
